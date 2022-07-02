d8a998e96b33d53d14d9bd810593c730011853f7
moliveros <mcoliveros1202@gmail.com>
2022-07-02 14:01:19 -0500

    created the enemy object, and methods for health, life, description, and damage

---
+++ __tests__/Enemy.test.js
@@ -0,0 +1,56 @@
+
+const Enemy = require('../lib/Enemy');
+const Potion = require('../lib/Potion');
+
+jest.mock('../lib/Potion');
+
+// test for enemy creation
+test('creates an enemy object', () => {
+    const enemy = new Enemy('goblin', 'sword');
+
+    expect(enemy.name).toBe('goblin');
+    expect(enemy.weapon).toBe('sword');
+    expect(enemy.health).toEqual(expect.any(Number));
+    expect(enemy.strength).toEqual(expect.any(Number));
+    expect(enemy.agility).toEqual(expect.any(Number));
+    expect(enemy.potion).toEqual(expect.any(Object));
+});
+
+// test for enemy health
+test("get enemy's health value", () => {
+    const enemy = new Enemy('goblin', 'sword');
+
+    expect(enemy.getHealth()).toEqual(expect.stringContaining(enemy.health.toString()));
+});
+
+// test for enemy life
+test('checks if enemy is alive or not', () => {
+    const enemy = new Enemy('goblin', 'sword');
+
+    expect(enemy.isAlive()).toBeTruthy();
+
+    enemy.health = 0;
+
+    expect(enemy.isAlive()).toBeFalsy();
+});
+
+// test for enemy's reduce health
+test("substracts from enemy's health", () => {
+    const enemy = new Enemy('goblin', 'sword');
+    const oldHealth = enemy.health;
+  
+    enemy.reduceHealth(5);
+  
+    expect(enemy.health).toBe(oldHealth - 5);
+  
+    enemy.reduceHealth(99999);
+  
+    expect(enemy.health).toBe(0);
+});
+
+test('gets a description of the enemy', () => {
+    const enemy = new Enemy('goblin', 'sword');
+
+    expect(enemy.getDescription()).toEqual(expect.stringContaining('goblin'));
+    expect(enemy.getDescription()).toEqual(expect.stringContaining('sword'));
+});
